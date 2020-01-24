cd41aee16db31b6ab50158605a206a4cf598beac
Runo-saduwa <orunosaduwa@gmail.com>
2020-01-24 23:03:14 +0100

    finished learning types

---
+++ types.js
@@ -0,0 +1,15 @@
+var myString;
+var myNum;
+var myBool;
+var myVar;
+var strArr;
+var numArr;
+var boolArr;
+var strNumTuple;
+myNum = 1 + 1;
+myString = 'heyyy' + 'kidsss';
+myBool = true;
+myVar = 5;
+strArr = ['hett', 'yoo'];
+numArr = [12, 3, 45, 67, 78, 9, 0];
+strNumTuple = ['boy', 1];
---
+++ types.ts
@@ -0,0 +1,22 @@
+let myString: string
+let myNum: number
+let myBool: boolean
+let myVar: any
+let strArr: string[]
+let numArr: number[]
+let boolArr: boolean[]
+let strNumTuple: [string, number]
+
+
+myNum = 1 + 1;
+myString = 'heyyy' + 'kidsss'
+myBool = true;
+myVar = 5;
+strArr = ['hett', 'yoo']
+numArr = [12,3,45,67,78,9,0];
+
+strNumTuple = ['boy', 1]
+
+
+
+
